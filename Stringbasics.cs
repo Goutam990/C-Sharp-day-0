8497802df3912a91318c0d438df5268a3cbea70e
Goutam Tiwari <102357588+Goutam990@users.noreply.github.com>
2025-09-01 17:38:36 +0530

    String Basics

---
+++ Stringbasics.cs
@@ -0,0 +1,14 @@
+ //  ==========================================
+            // C# - Strings
+            // Strings are Used to store --> text values. 
+            // Strings are the collection of characters enclose within doubled quotes.
+
+            string str1;
+            str1 = "helloEmployess";
+            Console.WriteLine(str1);
+            // to get the length of a string - .Length property is used - similar to java
+            Console.WriteLine("Length of given str1 is :- " + str1.Length);
+
+            // toUpper() & toLower() both are the methods
+            Console.WriteLine(str1.ToUpper());
+            Console.WriteLine(str1.ToLower());
