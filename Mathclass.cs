a54432f2e79930ec8a2330ff11d9f21057ab287a
Goutam Tiwari <102357588+Goutam990@users.noreply.github.com>
2025-09-01 17:24:52 +0530

    Math class - methods - to perform mathematical operation on integer value

---
+++ Mathclass.cs
@@ -0,0 +1,8 @@
+ // ============================================
+            // Math class has many methods. 
+            Console.WriteLine(Math.Max(5, 6));  //highest value of x and y
+            Console.WriteLine(Math.Min(3, 7));  // lowest value of x and y
+            Console.WriteLine(Math.Sqrt(64));   //Returns sq root of x
+            Console.WriteLine(Math.Abs(-234234)); //Return absolute +ive of x. 
+            Console.WriteLine(Math.Round(55.55)); // rounds off to nearest whole number.
+             
